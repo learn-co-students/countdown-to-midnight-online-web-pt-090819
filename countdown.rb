68a386af9c0b55e99c08637aa895994370b8a158
Lauren Dove <elledoveinc@gmail.com>
2019-09-14 00:50:21 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  #number = 10 -Realized the Lab wasnt asking for an actual number.
+  while number > 0
+   puts "#{number} SECOND(S)!"
+   number -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+  while number > 0 
+  puts "#{number} SECOND(S)!"
+  sleep 1 #CANT BELIEVE I GOT THIS RIGHT ON THR FIRST ATTEMPT!!!!!!
+  number -= 1 
+ end
 end
