0d31d2b5754b4af771c08e4f4d7458a338546d27
Jennifer Prince <jennifergraceprince@gmail.com>
2019-09-24 05:43:44 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
-#write your code here
+# hi wassup this is my code la la la
+def countdown(x)
+  x = 10 
+  while x > 0 
+   puts "#{x} SECOND(S)!"
+   x -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(x)
+  x = 10 
+  while x > 0 
+   puts "#{x} SECOND(S)!"
+   sleep(1)
+   x -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
